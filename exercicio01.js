a82c1e797400896b90aec83475f08572fd9317e2
MARIA EDUARDA BARRETO FIGUEREDO <44108865847@imaccommand024.local>
2025-11-19 11:36:56 -0300

    exercicios 01

--- exercicio01.js
+++ exercicio01.js
@@ -4,11 +4,13 @@ switch (login) {
         console.log("Susesso!")
         break;
 
+        
+
     default:
         console.log("Login ou senha incorretos")
         break;
 }
-let senha = ("Digite sua senha: ")
+let senha = prompt("Digite sua senha: ")
 switch (senha) {
     case "Senha123":
         console.log("Susesso!")
@@ -16,44 +18,5 @@ switch (senha) {
 
     default:
         console.log("Login ou senha incorretos")
-        break;
-}
-
-//-----------------------------------------------------------------//
-
-// exercicio02
-
-let semana = ("Digie um nmero para descobrir o sia da semana!")
-switch (semana) {
-    case "1":
-        console.log("Segunda-feira")
-        break;
-
-         case "2":
-        console.log("Terça-feira")
-        break;
-
-        case "3":
-        console.log("Quarta-feira")
-        break;
-
-        case "4":
-        console.log("Quinta-feira")
-        break;
-
-         case "5":
-        console.log("Sexta-feira")
-        break;
-
-         case "6":
-        console.log("Sabado")
-        break;
-
-        case "7":
-        console.log("Domingo")
-        break;
-
-    default:
-         console.log("Não existe este numero os dias da semana. Coloque um numero de 1 a 7")
         break;
 }
---
+++ exercicio02.js
@@ -0,0 +1,34 @@
+let semana = prompt("Digie um nmero para descobrir o sia da semana!")
+switch (semana) {
+    case "1":
+        console.log("Segunda-feira")
+        break;
+
+         case "2":
+        console.log("Terça-feira")
+        break;
+
+        case "3":
+        console.log("Quarta-feira")
+        break;
+
+        case "4":
+        console.log("Quinta-feira")
+        break;
+
+         case "5":
+        console.log("Sexta-feira")
+        break;
+
+         case "6":
+        console.log("Sabado")
+        break;
+
+        case "7":
+        console.log("Domingo")
+        break;
+
+    default:
+         console.log("Não existe este numero os dias da semana. Coloque um numero de 1 a 7")
+        break;
+}
